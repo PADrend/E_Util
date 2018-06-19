a32d88b34d6d3b79b81f6d1430dc7b7fbc8d6c2c
Sascha Brandt <myeti@mail.upb.de>
2018-06-19 14:28:15 +0200

    Macro for simplifying escript getters/setters

--- E_Utils.h
+++ E_Utils.h
@@ -106,6 +106,11 @@ class E_Utils {
 
 		static const converter_list_t & getConverters();
 };
-
 }
+
+//! Macro to simplify bindings for getter/setter methods
+#define ES_MGETSET(eclass, type, var)\
+  ES_MFUN(typeObject,const eclass,"get" #var, 0, 0, thisObj->get##var())\
+  ES_MFUN(typeObject,eclass,"set" #var,1,1, (thisObj->set##var(parameter[0].to<type>(rt)),thisEObj) )\
+	
 #endif // E_UTILS_H
